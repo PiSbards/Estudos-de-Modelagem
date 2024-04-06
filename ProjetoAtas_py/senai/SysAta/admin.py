29afd71594a0b5fffb0117bb0815544f16436287
Pietro Sbardellati <129914051+PiSbards@users.noreply.github.com>
2024-04-05 21:00:49 -0300

    Delete ProjetoAtas_py/senai directory

--- ProjetoAtas_py/senai/SysAta/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import Ata
-
-class ataAdmin(admin.ModelAdmin):
-    list_display = ('dataInicio', 'titulo')
-
-
-admin.site.register(Ata,ataAdmin)
--- ProjetoAtas_py/senai/SysAta/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Ata(models.Model):
-    dataInicio = models.DateField(auto_now=True)
-    dataFinal = models.DateField()
-    titulo = models.CharField(max_length=255)
-    pauta = models.TextField()
-    descricao = models.TextField('Descrição')
